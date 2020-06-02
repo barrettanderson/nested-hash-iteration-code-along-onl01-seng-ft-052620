04252ba9a7b16017384f518761984dfacc334e29
Barrett Anderson <barrettanderson1989@gmail.com>
2020-06-02 04:10:39 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -19,7 +19,7 @@ def remove_strawberry(contacts)
     if person == "Freedy Mercury"
       contact_details_hash.each do |attribute, data|
         binding.pry
-        end
+      end
     end
   end
 #   contacts.each do |person, contact_details_hash|
@@ -29,5 +29,4 @@ def remove_strawberry(contacts)
 #       end
 #     end
 # end
-
 end
